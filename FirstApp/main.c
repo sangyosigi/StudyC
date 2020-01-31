7f85520f7eea5ac128828673d4d5f4549c87409b
sangsik <sangsleahchi@gmail.com>
2020-01-31 11:35:52 +0900

    2장완료

--- FirstApp/main.c
+++ FirstApp/main.c
@@ -6,12 +6,13 @@
 #include <stdlib.h>
 
 int main() {
-	printf("Hello World!\n");
-	printf("12345678901234567890\n");
-	printf("My\tfriend\n");
-	printf("Goot\bd\tchance\n");
-	printf("Cow\rW\a\n");
-
+	printf("%d\n",10);
+	printf("%lf\n",3.4);
+	printf("%.1lf\n",3.45);
+	printf("%0.10lf\n",3.4);
+	
+	printf("%d과 %d의 합은 %d입니다.\n",10,20,10+20);
+	printf("%.1lf-%.1lf = %.1lf\n",3.4,1.2,3.4-1.2);
 	
 	//type here
 
